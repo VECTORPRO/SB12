733e3bec88eec18894044da934e225427ecbe1b2
VECTORPRO <Vector59rus@gmail.com>
2022-01-21 18:20:02 +0300

    Добавьте файлы проекта.

---
+++ SB12.cpp
@@ -0,0 +1,42 @@
+﻿
+#include <iostream>
+
+int globalsum = 0;
+
+void Print()
+{
+    std::cout << "Hello SkillBox!\n";
+}
+
+long long sum(long long a, long long b, long long c)
+{
+    //перменная тест равна 10
+    long long test = 10;
+    // возвращаем сумму a b c 
+    return (a + b + c);
+}
+
+int PrintInt(int ToPrint)
+{
+    std::cout << ToPrint << "\n";
+    return ToPrint;
+}
+
+int main()
+{
+    long long test1 = -100;
+    bool Probe = 100 == 100;
+
+    int a=5, b=10, c=15;
+
+    bool IsReady = true;
+    bool READY = false;
+
+    int SocketData;
+
+
+    long long summa = sum(a, b, c);
+
+    return 0;
+}
+
